c16de76f4d7a4dd1b999045fcf9262e9211a681c
Semyon Morozov <semyonmorozov@gmail.com>
2017-10-28 23:31:13 +0500

    Добавил класс CircularCloudLayouter, который необходимо реализовать.

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System;
+using System.Drawing;
+using System.Reflection;
+
+namespace TagsCloudVisualization
+{
+    class CircularCloudLayouter
+    {
+        public CircularCloudLayouter(Pointer center)
+        {
+            
+        }
+
+        Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
