d1ce12238429bb68563353670751bcfdfb19512b
Semyon Morozov <semyonmorozov@gmail.com>
2017-11-02 16:48:02 +0500

    Добавил возможность создавать спираль с центром в произваольной точке и со значениями по умолчанию.

--- TagsCloudVisualization/Spiral.cs
+++ TagsCloudVisualization/Spiral.cs
@@ -7,16 +7,35 @@ namespace TagsCloudVisualization
 {
     class Spiral
     {
-        private double alpha;
+        private Point center = new Point(0,0);
+        private double alpha = 0.1;
+
+        public Spiral(Point center, double alpha)
+        {
+            this.center = center;
+            this.alpha = alpha;
+        }
+
+        public Spiral(Point center)
+        {
+            this.center = center;
+        }
 
         public Spiral(double alpha)
         {
             this.alpha = alpha;
         }
 
+        public Spiral()
+        {
+            //Initialization spiral with default value of center and alpha
+        }
+
         public Point GetPoint(double angle)
         {
-            return new Point((int)(alpha * angle * Math.Cos(angle)), (int)(alpha * angle * Math.Sin(angle)));
+            int x = (int) (alpha * angle * Math.Cos(angle)) + center.X;
+            int y = (int) (alpha * angle * Math.Sin(angle)) + center.Y;
+            return new Point(x, y);
         }
     }
 
@@ -24,9 +43,9 @@ namespace TagsCloudVisualization
     public class Spiral_Should
     {
         [Test]
-        public void ReturnZeroPoint_WhenAngleIsZero()
+        public void ReturnZeroPoint_WhenSpiralIsDefault_AndAngleIsZero()
         {
-            var spiral = new Spiral(1);
+            var spiral = new Spiral();
             spiral.GetPoint(0).ShouldBeEquivalentTo(new Point(0,0));
         }
 
@@ -38,9 +57,20 @@ namespace TagsCloudVisualization
         [TestCase(2, 5, 2, -9)]
         [TestCase(2, 7, 10, 9)]
         [TestCase(2, 10, -16, -10)]
-        public void ReturnCorrectPoint(double alpha, double angle, int expectedX, int expectedY)
+        public void ReturnCorrectPoint_WithCustomAlpha_AndDefaultCenter(double alpha, double angle, int expectedX, int expectedY)
         {
             var spiral = new Spiral(alpha);
+            spiral.GetPoint(angle).ShouldBeEquivalentTo(new Point(expectedX, expectedY));
+        }
+
+        [TestCase(2, 0, 0, 0, 0, 0)]
+        [TestCase(2, 1, 0, 0, 1, 1)]
+        [TestCase(2, 1, 1, 1, 2, 2)]
+        [TestCase(2, 1, -1, -1, 0, 0)]
+        [TestCase(2, 7, -5, 6, 5, 15)]
+        public void ReturnCorrectPoint_WithCustomCenter_AndCustomAlpha(double alpha, double angle, int centerX, int centerY, int expectedX, int expectedY)
+        {
+            var spiral = new Spiral(new Point(centerX,centerY), alpha);
             spiral.GetPoint(angle).ShouldBeEquivalentTo(new Point(expectedX, expectedY));
         }
     }
