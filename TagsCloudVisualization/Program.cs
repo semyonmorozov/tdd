891de9b38037cd592e1c4ba161e5ebc2a2d2dbd9
Semyon Morozov <semyonmorozov@gmail.com>
2017-11-06 14:02:59 +0500

    Рефакторинг

--- TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/Program.cs
@@ -17,20 +17,20 @@ namespace TagsCloudVisualization
             var vizualizator = new TagsCloudVisualizator(screenBounds.Width,screenBounds.Height);
             using (var file = new System.IO.StreamReader(@"wordsStats.txt"))
             {
-                for (int i=0;i<400;i++)
+                for (var i=0;i<400;i++)
                 {
                     var line = file.ReadLine();
                     if (line == null) break;
                     line = Regex.Replace(line, "\t", " ");
-                    string[] splitedLine = line.Split(' ');
-                    string word = splitedLine[1];
+                    var splitedLine = line.Split(' ');
+                    var word = splitedLine[1];
                     var frequency = Int32.Parse(splitedLine[2]);
                     tags.Add(word,frequency);
                 }
             }
             vizualizator.AddTags(tags);
             var bitmap = vizualizator.Visualize(bgColor, textColor);
-            string path = @"C:\Temp\result.png";
+            var path = @"C:\Temp\result.png";
             bitmap.Save(path);
         }
     }
